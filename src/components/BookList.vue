698c33afb197c4c072572159fec6cefe550b2c8f
LarsPontus <38915153+LarsPontus@users.noreply.github.com>
2018-06-05 12:32:22 +0200

    adding the bookitem component

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -2,7 +2,7 @@
 <div>
 <h1>{{title}}</h1>
 <ul>
-    <li v-for="book in books">{{book.title}}:{{book.author}}</li>
+    <book-item v-for="book in books" :book='book'></book-item>
     
 </ul>
 
@@ -12,8 +12,12 @@
 </template>
 
 <script>
+import BookItem from './BookItem';
 export default {
 name: 'BookList',
+components: {
+    BookItem
+},
 data() {
 return {
  title: 'All Books',
